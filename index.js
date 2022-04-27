35df8b81193122dbd2d712125d53448f9d31f34a
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-04-27 20:49:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $("#btn-login").click(function () {
+    $("#background").show();
+    $("#popup-wrapper").show();
+  });
+  $("#cross").click(function () {
+    $("#background").hide();
+    $("#popup-wrapper").hide();
+  });
+  $("#background").click(function () {
+    $(this).hide();
+    $("#popup-wrapper").hide();
+  });
+});
