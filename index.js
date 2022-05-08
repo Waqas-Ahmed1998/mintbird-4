83caa6a2e79756efe9e5332b3e9f8bcd36fc13fd
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-05-09 02:11:57 +0500

    first fix commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-$(document).ready(function () {
-  $("#btn-login").click(function () {
-    $("#background").show();
-    $("#popup-wrapper").show();
-  });
-  $("#cross").click(function () {
-    $("#background").hide();
-    $("#popup-wrapper").hide();
-  });
-  $("#background").click(function () {
-    $(this).hide();
+// $(document).ready(function () {
+//   $("#btn-login").click(function () {
+//     $("#background").show();
+//     $("#popup-wrapper").show();
+//   });
+//   $("#cross").click(function () {
+//     $("#background").hide();
+//     $("#popup-wrapper").hide();
+//   });
+//   $("#background").click(function () {
+//     $(this).hide();
 
-    $("#popup-wrapper").hide();
-  });
-});
+//     $("#popup-wrapper").hide();
+//   });
+// });
